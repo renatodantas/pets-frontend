7123de86b3b9830eb424873b0962282b527166e0
Renato Dantas <rexdantas@gmail.com>
2018-12-27 14:02:42 -0200

    Adicionando tela de Listar Pets

--- src/components/ListarPets.vue
+++ src/components/ListarPets.vue
@@ -1,138 +1,43 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
+  <v-flex>
+    <v-data-table 
+      :headers="headers"
+      :items="pets"
+      class="elevation-2">
 
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <template slot="items" slot-scope="{ item }">
+          <td>{{ item.nome }}</td>
+          <td>{{ item.tipo.descricao }}</td>
+          <td>{{ item.dono.nome }}</td>
+        </template>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+        <template slot="pageText" slot-scope="props">
+          Linha {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
+        </template>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
+        <template slot="no-data">
+          Nenhum registro encontrado
+        </template>
 
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
-  </v-container>
+    </v-data-table>
+  </v-flex>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader'
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify'
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify'
-        }
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com'
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com'
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuetifyjs.com'
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs'
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify'
-        }
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer'
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined'
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-        }
-
-      ]
-    })
-  }
+export default {
+  name: 'ListarPets',
+  props: {
+    pets: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    headers: [
+      { text: 'Nome', align: 'left', value: 'nome' },
+      { text: 'Tipo', align: 'left', value: 'tipo.descricao' },
+      { text: 'Dono', align: 'left', value: 'dono.nome' },
+    ]
+  })
+}
 </script>
-
-<style>
-
-</style>
--- src/views/Pets.vue
+++ src/views/Pets.vue
@@ -1,5 +1,32 @@
 <template>
-  <div class="about">
-    <h1>This is an Pets page</h1>
-  </div>
+  <v-container>
+    <div class="title">Listar Pets</div>
+
+    <v-layout>
+      <v-flex>
+        <ListarPets :pets="pets"/>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import ListarPets from '../components/ListarPets'
+
+export default {
+  components: {
+    ListarPets
+  },
+  data() {
+    return {
+      pets: []
+    }
+  },
+  created() {
+    fetch('http://localhost:8080/api/pets')
+      .then(res => res.json())
+      .then(json => this.pets = json)
+      .then(() => console.log(this.pets))
+  }
+}
+</script>
